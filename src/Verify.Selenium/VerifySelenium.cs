063d57046aeb6fed3844b3c348d3b079ae0c45d6
Simon Cropp <simon.cropp@gmail.com>
2023-09-13 23:08:54 +1000

    cleanup

--- src/Verify.Selenium/VerifySelenium.cs
+++ src/Verify.Selenium/VerifySelenium.cs
@@ -6,10 +6,6 @@ public static class VerifySelenium
 {
     public static bool Initialized { get; private set; }
 
-    [Obsolete("Use Initialize()")]
-    public static void Enable() =>
-        Initialize();
-
     public static void Initialize()
     {
         if (Initialized)
